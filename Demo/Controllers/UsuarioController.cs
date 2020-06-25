aced827166387c2b1767fa79fec47579fb7c040e
jlrcobos <jlrcobos@gmail.com>
2020-06-25 18:05:22 -0500

    Carga inicial

---
+++ Demo/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+namespace Demo.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Guardar(Usuario dto)
+        {
+            try
+            {
+                throw new Exception(" Sucedio un error");
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message =ex.Message });
+            }
+            return Json(new { success = true,message="Registro correcto" });
+        }
+    }
+}
---
+++ Demo/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Usuario
+    {
+      public  string nombres { get; set; }
+      public string apellidos { get; set; }
+        public Usuario()
+        {
+
+        }
+        public Usuario(string nombres,string apellidos)
+        {
+            this.nombres = nombres;
+        }
+    }
+}
